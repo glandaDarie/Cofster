96d2ffd8c46b76d16d8f7f2fb9b4b1c6b0fa2f2c
darie.glanda <darieglanda@outlook.com>
2023-08-28 18:27:49 +0300

    added backend code to get all the purchases made by the user given his email

---
+++ mobile_app_backend/getUserPurchaseHistory.js
@@ -0,0 +1,47 @@
+const AWS = require("aws-sdk");
+const REGION = "us-east-1";
+const TABLE_NAME = "purchaseHistory";
+AWS.config.update({region : REGION});
+
+exports.handler = async (event) => {
+  let requestBody = null;
+  try {
+    requestBody = event.body;  
+  } catch(error) {
+    return {
+      statusCode: 400,
+      body: "Invalid request body " + error
+    };
+  }
+  
+  const getParams = {
+    TableName: TABLE_NAME
+  };
+	
+  try {
+        const database = new AWS.DynamoDB.DocumentClient({region: REGION});
+        const scanResult = await database.scan(getParams).promise();
+        const emails = scanResult.Items[0]["emails"];
+        
+        for(let i = 0; i < emails.length; ++i) {
+            if(emails[i]["email"] === requestBody["email"]) {
+                return {
+                    statusCode: 200,
+                    body: {
+                    email: requestBody["email"],
+                    orderInformation: emails[i]["orderInformation"]
+                    }
+                };
+            }
+        }
+        return {
+            statusCode: 404,
+		    body: "No order information found for the user"
+        }
+    } catch(error) {
+        return {
+            statusCode: 500,
+            body: "Could not fetch the data from DynamoDB table: " + TABLE_NAME + ", error: "+ error
+        };
+    };
+};
