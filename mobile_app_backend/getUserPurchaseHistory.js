d0e616e7f02deacdaff8b390b19ba0a35e6825ff
darie.glanda <darieglanda@outlook.com>
2023-08-29 13:47:46 +0300

    added code in postUserPurchase.js for a new user that purchased a coffee drink. Perfomed also code formatting

--- mobile_app_backend/getUserPurchaseHistory.js
+++ mobile_app_backend/getUserPurchaseHistory.js
@@ -1,55 +1,55 @@
 const AWS = require("aws-sdk");
 const REGION = "us-east-1";
 const TABLE_NAME = "purchaseHistory";
-AWS.config.update({region : REGION});
+AWS.config.update({ region: REGION });
 
 exports.handler = async (event) => {
   let requestBody = null;
   try {
-    requestBody = event.body;  
-  } catch(error) {
+    requestBody = event.body;
+  } catch (error) {
     return {
       statusCode: 400,
       body: "Invalid request body " + error
     };
   }
-  
+
   const getParams = {
     TableName: TABLE_NAME
-	};
-	
+  };
+
   try {
-      const database = new AWS.DynamoDB.DocumentClient({region: REGION});
-	    const scanResult = await database.scan(getParams).promise();
-	    const emails = scanResult.Items[0]["emails"];
-	    
-	    for(let i = 0; i < emails.length; ++i) {
-	      if(emails[i]["email"] === requestBody["email"]) {
-	        return {
-	          statusCode: 200,
-	          body: {
-	            email: requestBody["email"],
-	            positionFound: +i,
-	            orderInformation: emails[i]["orderInformation"]
-	          }
-	        };
-	      }
-	    }
-	    
-	    return requestBody["showOrderHistory"] === 1 ? 
-	    {
-	      statusCode: 404,
-	      showOrderHistory: scanResult.Items,
-	      body: "No order information found for the user"
-	    } : 
-	    {
-	      statusCode: 404,
-	      body: "No order information found for the user"
-	    }
-    } catch(error) {
+    const database = new AWS.DynamoDB.DocumentClient({ region: REGION });
+    const scanResult = await database.scan(getParams).promise();
+    const emails = scanResult.Items[0]["emails"];
+
+    for (let i = 0; i < emails.length; ++i) {
+      if (emails[i]["email"] === requestBody["email"]) {
         return {
-            statusCode: 500,
-            body: "Could not fetch the data from DynamoDB table: " + TABLE_NAME + ", error: "+ error
+          statusCode: 200,
+          body: {
+            email: requestBody["email"],
+            positionFound: +i,
+            orderInformation: emails[i]["orderInformation"]
+          }
         };
+      }
+    }
+
+    return requestBody["showOrderHistory"] === 1 ?
+      {
+        statusCode: 404,
+        showOrderHistory: scanResult.Items,
+        body: "No order information found for the user"
+      } :
+      {
+        statusCode: 404,
+        body: "No order information found for the user"
+      }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Could not fetch the data from DynamoDB table: " + TABLE_NAME + ", error: " + error
     };
+  };
 };
--- mobile_app_backend/postUserPurchase.js
+++ mobile_app_backend/postUserPurchase.js
@@ -17,7 +17,8 @@ exports.handler = async (event) => {
   const payload = {
     body: {
       email: requestBody["email"],
-    },
+      "showOrderHistory": 1
+    }
   };
 
   const getPurchaseHistoryFunctionParams = {
@@ -25,28 +26,28 @@ exports.handler = async (event) => {
     InvocationType: "RequestResponse",
     Payload: JSON.stringify(payload),
   };
-  
+
   const lambda = new AWS.Lambda({ region: REGION });
   const response = await lambda
     .invoke(getPurchaseHistoryFunctionParams)
     .promise();
-  
+
   const statusCode = JSON.parse(response.Payload)["statusCode"];
-  const positionFound = JSON.parse(response.Payload).body["positionFound"];
   let database = new AWS.DynamoDB.DocumentClient({ region: REGION });
-  
+
   if (statusCode === 200) {
+    const positionFound = JSON.parse(response.Payload).body["positionFound"];
     let historyPurchase = JSON.parse(response.Payload).body["orderInformation"];
-    let lastKey = Object.keys(historyPurchase[historyPurchase.length - 1]).toString();
-    let lastKeySplitted = lastKey.split("_");
-    let lastKeyName = lastKeySplitted[0];
+    const lastKey = Object.keys(historyPurchase[historyPurchase.length - 1]).toString();
+    const lastKeySplitted = lastKey.split("_");
+    const lastKeyName = lastKeySplitted[0];
     let lastKeyIndex = +lastKeySplitted.slice(-1)[0]
     lastKeyIndex += 1;
     let newPurchaseKey = [lastKeyName, lastKeyIndex].join("_");
-    
+
     const newPurchase = {
       coffeeCupSize: requestBody["coffeeCupSize"],
-      coffeeName: requestBody["coffeeCupSize"], 
+      coffeeName: requestBody["coffeeCupSize"],
       coffeeNumberOfIceCubes: requestBody["coffeeNumberOfIceCubes"],
       coffeeNumberOfSugarCubes: requestBody["coffeeNumberOfSugarCubes"],
       coffeePrice: requestBody["coffeePrice"],
@@ -54,13 +55,13 @@ exports.handler = async (event) => {
       coffeeTemperature: requestBody["coffeeTemperature"],
       hasCoffeeCream: requestBody["hasCoffeeCream"]
     };
-    
-    const historyPurchaseConcatenated = historyPurchase.concat({[newPurchaseKey]: newPurchase});
-    
+
+    const historyPurchaseConcatenated = historyPurchase.concat({ [newPurchaseKey]: newPurchase });
+
     const updateParams = {
       TableName: TABLE_NAME,
       Key: {
-        purchaseHistoryId: "1", 
+        purchaseHistoryId: "1",
       },
       UpdateExpression: "SET #emails[" + positionFound + "].#orderInformation = :history",
       ExpressionAttributeNames: {
@@ -71,7 +72,7 @@ exports.handler = async (event) => {
         ":history": historyPurchaseConcatenated,
       },
     };
-  
+
     try {
       await database.update(updateParams).promise();
     } catch (error) {
@@ -80,11 +81,59 @@ exports.handler = async (event) => {
         body: "Could not add new purchase to purchase history, error: " + error,
       };
     }
+
     return {
       statusCode: 201,
       body: "Successfully added new purchase to purchase history",
     };
   } else {
-    console.log("I am somewhere else");
+    const orderHistory = JSON.parse(response.Payload)["showOrderHistory"];
+    console.log(JSON.stringify(orderHistory));
+    const purchasesOfUsers = orderHistory[0]["emails"];
+
+    const newPurchase = {
+      email: requestBody["email"],
+      orderInformation: [
+        {
+          purchase_1: {
+            coffeeCupSize: requestBody["coffeeCupSize"],
+            coffeeName: requestBody["coffeeCupSize"],
+            coffeeNumberOfIceCubes: requestBody["coffeeNumberOfIceCubes"],
+            coffeeNumberOfSugarCubes: requestBody["coffeeNumberOfSugarCubes"],
+            coffeePrice: requestBody["coffeePrice"],
+            coffeeQuantity: requestBody["coffeeQuantity"],
+            coffeeTemperature: requestBody["coffeeTemperature"],
+            hasCoffeeCream: requestBody["hasCoffeeCream"]
+          }
+        }
+      ]
+    }
+
+    const updateParams = {
+      TableName: TABLE_NAME,
+      Key: {
+        purchaseHistoryId: "1"
+      },
+      UpdateExpression: "SET #emails[" + purchasesOfUsers.length + "] = :newPurchase",
+      ExpressionAttributeNames: {
+        "#emails": "emails"
+      },
+      ExpressionAttributeValues: {
+        ":newPurchase": newPurchase,
+      },
+    };
+
+    try {
+      await database.update(updateParams).promise();
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Could not add new purchase to purchase history, error: " + error
+      };
+    }
+    return {
+      statusCode: 201,
+      body: "Successfully added new user that purchased a drink to purchase history",
+    };
   }
 };
