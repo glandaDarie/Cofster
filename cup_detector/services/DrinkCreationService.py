5645dc351109c0398956d0215843c047bedb379b
darie.glanda <darieglanda@outlook.com>
2023-09-16 22:42:36 +0300

    fixed a thread bug when an interruption happens, it should make the drink only if the drink is present

--- cup_detector/services/DrinkCreationService.py
+++ cup_detector/services/DrinkCreationService.py
@@ -1,7 +1,7 @@
-from typing import Dict, Callable, Generator
+from typing import List, Dict, Callable, Generator
 from helpers.coffee_machine_controller import CoffeeMachineController
 import concurrent.futures
-from threading import Event
+from threading import Event, Lock
 from utils.logger import thread_information_logger
 
 class DrinkCreationSevice(CoffeeMachineController):
@@ -17,8 +17,10 @@ class DrinkCreationSevice(CoffeeMachineController):
                     self.stop_drink_creation_event.clear()
                     self.stop_continuous_cup_checking.clear()
                     futures : Dict[concurrent.futures.ThreadPoolExecutor, str] = {}
-                    futures[executor.submit(lambda : self.__create_drink(drink_information=drink_information))] = "create_drink"
-                    futures[executor.submit(lambda : self.__continuously_check_cup(callback_cup_detection=callback_cup_detection))] = "continuously_check_cup"
+                    futures[executor.submit(lambda : self.__create_drink(drink_information=drink_information, \
+                            callback_cup_detection=callback_cup_detection))] = "create_drink"
+                    futures[executor.submit(lambda : self.__continuously_check_cup(callback_cup_detection=callback_cup_detection))] = \
+                            "continuously_check_cup"
                     
                     for future in concurrent.futures.as_completed(futures):
                         try:
@@ -49,16 +51,16 @@ class DrinkCreationSevice(CoffeeMachineController):
             else:
                 print("Cup not detected, stopping coffee creation.")
 
-    def __create_drink(self, drink_information : Dict[str, str]) -> bool | str:
+    def __create_drink(self, drink_information : Dict[str, str], callback_cup_detection : Callable[[bool], bool]) -> bool | str:
         while True:
             try:
                 is_drink_creation_interrupted : bool = self.stop_drink_creation_event.wait(timeout=10)
                 # do the drink creation here
                 # print(drink_information)
                 # need to also monitor if the cup was detected throughout the 10 seconds that the state didn't change
-                if is_drink_creation_interrupted:
+                if is_drink_creation_interrupted or not callback_cup_detection():
                     print(f"is_drink_creation_interrupted: {is_drink_creation_interrupted}")
-                    # self.stop_drink_creation_event.clear()
+                    self.stop_drink_creation_event.clear()
                     continue
             except Exception as e:
                 return f"Error, cup is moved again from the normal position: {e}"
@@ -68,7 +70,6 @@ class DrinkCreationSevice(CoffeeMachineController):
     def __continuously_check_cup(self, callback_cup_detection : Callable[[bool], bool]) -> Generator:
         while not self.stop_continuous_cup_checking.is_set():
             if callback_cup_detection():
-                self.stop_drink_creation_event.clear()
                 yield "Cup detected"            
             else:
                 self.stop_drink_creation_event.set()
