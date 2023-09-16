3bad2c001455216fa8e17eeabc3177effe84a487
darie.glanda <darieglanda@outlook.com>
2023-09-16 22:49:38 +0300

    added docstrings for drinkCreationService

--- cup_detector/services/DrinkCreationService.py
+++ cup_detector/services/DrinkCreationService.py
@@ -5,6 +5,24 @@ from threading import Event, Lock
 from utils.logger import thread_information_logger
 
 class DrinkCreationSevice(CoffeeMachineController):
+    """
+    A service for simulating drink creation in a coffee machine.
+
+    This service uses threads to simulate the creation of drinks in a coffee machine.
+    It monitors cup detection and creates drinks when a cup is detected.
+
+    Args:
+        None
+
+    Attributes:
+        stop_drink_creation_event (Event): An event used to control drink creation threads.
+        stop_continuous_cup_checking (Event): An event used to control cup detection monitoring.
+
+    Methods:
+        simulate_creation: Public method to simulate the creation of drinks based on cup detection.
+        __create_drink: Private method to create drinks.
+        __continuously_check_cup: Private method to continuously monitor cup detection.
+    """
     def __init__(self):
         super().__init__()
         self.stop_drink_creation_event : Event = Event()
@@ -21,7 +39,6 @@ class DrinkCreationSevice(CoffeeMachineController):
                             callback_cup_detection=callback_cup_detection))] = "create_drink"
                     futures[executor.submit(lambda : self.__continuously_check_cup(callback_cup_detection=callback_cup_detection))] = \
                             "continuously_check_cup"
-                    
                     for future in concurrent.futures.as_completed(futures):
                         try:
                             result_drink_creation : bool|Generator = future.result()                                            
@@ -31,18 +48,6 @@ class DrinkCreationSevice(CoffeeMachineController):
                                     executor.shutdown()
                                     thread_information_logger(futures)
                                     break
-                            # # implementation is still dummy here, need to remove the if statement
-                            # if isinstance(result_drink_creation, Generator):
-                            #     for result_generator_item in result_drink_creation:
-                            #         print(f"Result: {result_generator_item}")
-                            #         if result_generator_item == "Cup not detected":
-                            #             pass
-                            # elif isinstance(result_drink_creation, bool):
-                            #     print(f"Result: {result_drink_creation}")
-                            #     if result_drink_creation:
-                            #         executor.shutdown()
-                            #         thread_information_logger(futures)
-                            #         break
                         except InterruptedError as e:  
                             return f"Error from thread: {e}"                         
                 print("Coffee creation complete.")
@@ -52,12 +57,22 @@ class DrinkCreationSevice(CoffeeMachineController):
                 print("Cup not detected, stopping coffee creation.")
 
     def __create_drink(self, drink_information : Dict[str, str], callback_cup_detection : Callable[[bool], bool]) -> bool | str:
+        """
+        Private thread method to create drinks.
+
+        Args:
+            drink_information (Dict[str, str]): Information about the drink that is to be created.
+            callback_cup_detection (Callable[[bool], bool]): A callback function to detect the presence of a cup.
+
+        Returns:
+            bool | str: True if drink creation is successful, an error message otherwise. 
+                        Interruptions happen if the cup is not present (moved) from the specific position.
+        """
         while True:
             try:
                 is_drink_creation_interrupted : bool = self.stop_drink_creation_event.wait(timeout=10)
                 # do the drink creation here
                 # print(drink_information)
-                # need to also monitor if the cup was detected throughout the 10 seconds that the state didn't change
                 if is_drink_creation_interrupted or not callback_cup_detection():
                     print(f"is_drink_creation_interrupted: {is_drink_creation_interrupted}")
                     self.stop_drink_creation_event.clear()
@@ -68,6 +83,15 @@ class DrinkCreationSevice(CoffeeMachineController):
             return True
 
     def __continuously_check_cup(self, callback_cup_detection : Callable[[bool], bool]) -> Generator:
+        """
+        Private thread method to continuously monitor cup detection.
+
+        Args:
+            callback_cup_detection (Callable[[bool], bool]): A callback function to detect the presence of the respective cup.
+
+        Yields:
+            str: "Cup detected" if the cup is detected, "Cup not detected" otherwise.
+        """
         while not self.stop_continuous_cup_checking.is_set():
             if callback_cup_detection():
                 yield "Cup detected"            
