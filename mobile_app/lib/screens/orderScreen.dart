443613cc1bfb9ecd461d46648e0274e9877712ba
darie.glanda <darieglanda@outlook.com>
2023-10-16 19:51:16 +0300

    removed some non functional code inside the orderScreen.dart

--- mobile_app/lib/screens/orderScreen.dart
+++ mobile_app/lib/screens/orderScreen.dart
@@ -1,9 +1,6 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
-import 'package:coffee_orderer/services/updateProviderService.dart'
-    show UpdateProvider;
-import 'package:provider/provider.dart';
 import 'package:coffee_orderer/models/orderInformation.dart'
     show OrderInformation;
 import 'package:coffee_orderer/utils/displayContentCards.dart'
@@ -48,54 +45,60 @@ class _OrderPageState extends State<OrderPage> {
   Widget build(BuildContext context) {
     final DatabaseReference reference =
         FirebaseDatabase.instance.ref().child("Orders");
-    return Consumer<UpdateProvider>(
-      builder:
-          (BuildContext context, UpdateProvider updateProvider, Widget child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              "            Orders",
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.normal,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "            Orders",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        backgroundColor: Colors.brown.shade700,
+      ),
+      body: FirebaseAnimatedList(
+        query: reference,
+        shrinkWrap: true,
+        itemBuilder: (BuildContext context, DataSnapshot snapshot,
+            Animation<double> animation, int index) {
+          String coffeeInfromation = snapshot.value.toString();
+          String parsedCoffeeInformation = coffeeInfromation
+              .replaceAll(
+                  RegExp(r"{|}"
+                      "|coffeeCupSize: "
+                      "|coffeeFinishTimeEstimation: "
+                      "|coffeeName: "
+                      "|coffeeOrderTime: "
+                      "|coffeePrice: "
+                      "|coffeeStatus: "
+                      "|coffeeTemperature: "
+                      "|communication: "
+                      "|hasCream: "
+                      "|numberOfIceCubes: "
+                      "|numberOfSugarCubes: "
+                      "|quantity: "),
+                  "")
+              .trim();
+          this._orderList = parsedCoffeeInformation.split(",");
+          return displayContentCards(
+              OrderInformation(
+                coffeeTemperature: this._orderList[0],
+                hasCream: this._orderList[1] == "true" ? true : false,
+                coffeeName: this._orderList[2],
+                quantity: int.tryParse(this._orderList[3]),
+                numberOfIceCubes: int.tryParse(this._orderList[4]),
+                coffeeCupSize: this._orderList[5],
+                coffeeEstimationTime: this._orderList[6],
+                numberOfSugarCubes: int.tryParse(this._orderList[7]),
+                coffeeStatus: int.tryParse(this._orderList[8]),
+                communication: this._orderList[9],
+                coffeePrice: this._orderList[10],
+                coffeeOrderTime: this._orderList[11],
               ),
-            ),
-            backgroundColor: Colors.brown.shade700,
-          ),
-          body: FirebaseAnimatedList(
-            query: reference,
-            shrinkWrap: true,
-            itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                Animation<double> animation, int index) {
-              String coffeeInfromation = snapshot.value.toString();
-              String parsedCoffeeInformation = coffeeInfromation
-                  .replaceAll(
-                      RegExp(
-                          "{|}|coffeeCupSize: |coffeeFinishTimeEstimation: |coffeeName: |coffeeOrderTime: |coffeePrice: |coffeeStatus: |coffeeTemperature: |communication: |hasCream: |numberOfIceCubes: |numberOfSugarCubes: |quantity: "),
-                      "")
-                  .trim();
-              this._orderList = parsedCoffeeInformation.split(",");
-              return displayContentCards(
-                  OrderInformation(
-                    coffeeTemperature: this._orderList[0],
-                    hasCream: this._orderList[1] == "true" ? true : false,
-                    coffeeName: this._orderList[2],
-                    quantity: int.tryParse(this._orderList[3]),
-                    numberOfIceCubes: int.tryParse(this._orderList[4]),
-                    coffeeCupSize: this._orderList[5],
-                    coffeeEstimationTime: this._orderList[6],
-                    numberOfSugarCubes: int.tryParse(this._orderList[7]),
-                    coffeeStatus: int.tryParse(this._orderList[8]),
-                    communication: this._orderList[9],
-                    coffeePrice: this._orderList[10],
-                    coffeeOrderTime: this._orderList[11],
-                  ),
-                  cardFlipParams: [_flipCard, _isCardFlipped, index]);
-            },
-          ),
-        );
-      },
+              cardFlipParams: [_flipCard, _isCardFlipped, index]);
+        },
+      ),
     );
   }
 }
