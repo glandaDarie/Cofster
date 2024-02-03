68eb2d1853585134e93fd38f60726eb1ae55c16c
darie.glanda <darieglanda@outlook.com>
2024-02-03 22:26:36 +0200

    added the more libraries needed for ingredient_updater to the requirements.txt

--- cup_detection/microservices/ingredients_updater/utils/paths.py
+++ cup_detection/microservices/ingredients_updater/utils/paths.py
@@ -1,3 +1,3 @@
 import os
 
-COFFEE_CREATION_PATH : str = os.path.join(os.path.dirname(os.getcwd()), "assets", "coffee_creation_data.txt")
+COFFEE_CREATION_PATH : str = os.path.join(os.getcwd(), "assets", "coffee_creation_data.txt")
