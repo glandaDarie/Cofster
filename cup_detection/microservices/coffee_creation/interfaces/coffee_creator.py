6f6f2fef54a52a9f5b0894dbfb1ce89812175f25
darie.glanda <darieglanda@outlook.com>
2024-04-15 16:53:44 +0300

    removed unnecessary implementation on CoffeeCreator abstract method

--- cup_detection/microservices/coffee_creation/interfaces/coffee_creator.py
+++ cup_detection/microservices/coffee_creation/interfaces/coffee_creator.py
@@ -5,8 +5,8 @@ from interfaces.type_coffee_creator_service import TypeCoffeeCreatorService
 class CoffeeCreator(ABC):
     @abstractmethod
     def __init__(self, parallel_coffee_creator_service : TypeCoffeeCreatorService):
-        self.parallel_coffee_creator_service : TypeCoffeeCreatorService = parallel_coffee_creator_service
-
+        pass
+    
     @abstractmethod
     def do(self, coffee_data : Dict[str, Any], *, pins : List[int] | None = None) -> Tuple[str]:
         pass
